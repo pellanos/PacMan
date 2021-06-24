af405da5fe31a0854f43bc85e86dbc0148fa6488
pellanos <81934994+pellanos@users.noreply.github.com>
2021-06-24 11:18:32 -0400

    Add files via upload

---
+++ pacman.js
@@ -0,0 +1,32 @@
+var pos = 0;
+const pacArray = [
+    ['images/PacMan1.png', 'images/PacMan2.png'],
+    ['images/PacMan3.png', 'images/PacMan4.png']
+];
+var direction = 0;
+var focus = 0;
+
+function Run() {
+    let img = document.getElementById("PacMan");
+    let imgWidth = img.width
+    focus = (focus + 1) % 2;
+    direction = checkPageBounds(direction, imgWidth);
+    img.src = pacArray[direction][focus];
+    if (direction) {
+        pos -= 20;
+        img.style.left = pos + "px";
+    } else {
+        pos += 20;
+        img.style.left = pos + 'px';
+    }
+    setTimeout(Run, 200);
+}
+
+function checkPageBounds(direction, imgWidth) {
+    let windowWidth = window.innerWidth;
+    if (pos + imgWidth >= windowWidth)
+        direction = 1;
+    else if (pos <= 0)
+        direction = 0;
+    return direction;
+}
